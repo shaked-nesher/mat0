294b9c0218b424f188482154b550f4ff5c06b053
shaked-nesher <shakednesher@gmail.com>
2021-03-08 17:23:30 +0200

    new_add

---
+++ add.py
@@ -0,0 +1,7 @@
+number1=input("please enter the first number:")
+number1=int(number1)
+number2=input("please enter the secend number:")
+number2=int(number2)
+sum=number1+number2
+print("the sum is:",sum)
+
